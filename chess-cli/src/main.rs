4941132cb8619a3739fedafb0ff9f7219405192d
Ryan Norris <rnorris@palantir.com>
2021-04-25 00:37:51 +0900

    Add divide CLI

--- chess-cli/src/main.rs
+++ chess-cli/src/main.rs
@@ -1,3 +1,38 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(version = "0.1", author = "Ryan N. <rynorris@gmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Divide(Divide),
+}
+
+#[derive(Clap)]
+struct Divide {
+    #[clap(short)]
+    fen: String,
+
+    #[clap(short)]
+    depth: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    match opts.subcmd {
+        SubCommand::Divide(div) => {
+            let state = chess_lib::fen::load_fen(&div.fen);
+            let counts = chess_lib::perft::divide(&state, div.depth);
+            let mut lines: Vec<String> = counts.iter().map(|(k, v)| {
+                return format!("{}: {}", k, v);
+            }).collect();
+            lines.sort();
+            lines.iter().for_each(|l| println!("{}", l));
+        },
+    }
 }
